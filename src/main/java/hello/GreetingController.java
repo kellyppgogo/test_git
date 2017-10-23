ae244cc173fb9ee5ace2380060f58eb718b2c4d8
kellyppgogo <312200746@qq.com>
2017-10-23 18:25:26 +0800

    add log

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,18 +1,24 @@
 package hello;
 
-import java.util.concurrent.atomic.AtomicLong;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
-public class GreetingController {
+public class GreetingController{
+
+    private static Logger logger = LoggerFactory.getLogger(GreetingController.class);
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        logger.info("greeting from {}", name);
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
